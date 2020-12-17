b0814e88fe0e9596fc22450efc51c885b1e92d9b
andreyscott <62455784+andreyscott@users.noreply.github.com>
2020-12-18 00:46:40 +0100

    making changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+import Footer from './Footer.js';
+import './App.css';
+
+function App() {
+  return(
+    <div className="app">
+      <Footer />
+    </div>
+  )
+  
+}
+
+export default App;
